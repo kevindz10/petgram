20df66b08eba03d586d661ed9c87e572a36be674
Kevin Diaz <kdiaz@almacenesanfora.com.mx>
2019-08-09 12:27:31 -0500

    agregando globalStyles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react'
-// import { Category } from './components/Category'
 import { ListOfCategories } from './components/ListOfCategories'
+import { GlobalStyle } from './GlobalStyles'
 
 export const App = () => (
-  <ListOfCategories />
+  <div>
+    <GlobalStyle />
+    <ListOfCategories />
+  </div>
 )
